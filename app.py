e127cb6c4c140ff27b7082775bc1825656901d19
João Vitor Dysarz <dysarzjoaovitor@gmail.com>
2024-09-01 18:17:14 -0300

    teste

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db= SQLAlchemy(app)
+
+# Modelagem 
+# Produto (id, name, price, description)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price= db.Column(db.Float,nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+
+# Definir uma rota raiz da nossa página inicial e a função que será executada ao requisitar
+
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+if __name__== "__main__":
+    app.run(debug=True)
